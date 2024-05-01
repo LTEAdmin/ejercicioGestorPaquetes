b0ab6ff18ae31d4b696d21ec761c516b1c7a957d
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-01 11:10:45 -0400

    prueba y muestra uso de uuid V4

--- uuid.js
+++ uuid.js
@@ -4,7 +4,18 @@
 
 import { v4 as uuidv4 } from 'uuid'; // importa el modulo uuid
 
+// forma normal con 32 caracteres
 const id = uuidv4();
+// forma de recortar el id, valor inicial y valor final
+const id2 = uuidv4().slice(0, 15);
 console.log(id);
-console.log(uuidv4());
-console.log(uuidv4());      
+console.log(id2);
+console.log(uuidv4());  
+
+// forma de definir id en un objeto, ojo cada vez que se ejecuta crea un id distinto, tener cuidado de no ingresar dos veces a una persona con el id distinto
+const persona = {
+    id: uuidv4().slice(0, 8),
+    name: "Alejandra",
+    age: 25,
+}
+console.log(persona)
