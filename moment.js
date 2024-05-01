60b944c7383f28158a71be43ba7703c1c8665fb1
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-01 13:45:27 -0400

    Prueba de trrabajo con fechas y horas correctamente

--- moment.js
+++ moment.js
@@ -6,3 +6,15 @@ console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
 console.log(moment().format("MMMM Do YYYY, h:mm:ss a"))
 console.log(moment().format("h:mm:ss a")   )
 console.log(moment().format("dd-MM-YYYY, h:mm:ss a"))
+
+const formato = 'dddd, Do MMMM YYYY, h:mm:ss a';
+const hoy = moment();
+
+
+//permite mostrar una tabla en consola
+console.table({
+  hoy: hoy.format(formato),
+  ayer: hoy.subtract(1, 'days').format(formato),
+  manana: hoy.add(1, 'days').format(formato),
+  'dos semanas': hoy.add(2, 'weeks').format(formato),
+});
